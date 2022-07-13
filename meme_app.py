ddaa1f2eb10430922bab74d743571dbb16a79efd
armalite <ttc_rulz@hotmail.com>
2022-07-13 15:17:28 +1200

    Remove redundant bits

--- meme_app.py
+++ meme_app.py
@@ -8,14 +8,13 @@ from github import Github, GithubIntegration
 app = Flask(__name__)
 
 app_id = '219152'
-# Read the bot certificate
+
 with open(
         os.path.normpath(os.path.expanduser('pr-meme-bot-42-private-key.pem')),
         'r'
 ) as cert_file:
     app_key = cert_file.read()
 
-# Create an GitHub integration instance
 git_integration = GithubIntegration(
     app_id,
     app_key,
@@ -24,10 +23,10 @@ git_integration = GithubIntegration(
 
 @app.route("/", methods=['POST'])
 def bot():
-    # Get the event payload
+
     payload = request.json
 
-    # Check if the event is a GitHub PR creation event
+    # Check if it is a GitHub PR event
     if not all(k in payload.keys() for k in ['action', 'pull_request']) and \
             payload['action'] == 'opened':
         return "ok"
@@ -35,9 +34,7 @@ def bot():
     owner = payload['repository']['owner']['login']
     repo_name = payload['repository']['name']
 
-    # Get a git connection as our bot
-    # Here is where we are getting the permission to talk as our bot and not
-    # as a Python webservice
+    # Get git connection as our bot
     git_connection = Github(
         login_or_token=git_integration.get_access_token(
             git_integration.get_installation(owner, repo_name).id
@@ -47,14 +44,13 @@ def bot():
 
     issue = repo.get_issue(number=payload['pull_request']['number'])
 
-    # Call meme-api to get a random meme
+    # Call meme-api to get random meme
     response = requests.get(url='https://meme-api.herokuapp.com/gimme')
     if response.status_code != 200:
         return 'ok'
 
-    # Get the best resolution meme
     meme_url = response.json()['preview'][-1]
-    # Create a comment with the random meme
+
     issue.create_comment(f"![Alt Text]({meme_url})")
     return "ok"
 
